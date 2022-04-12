baae1aaaf64757c3af81e6bab7b10eeeefbe03fd
marcinkalaus <kalaus@student.agh.edu.pl>
2022-04-13 00:14:16 +0200

    Initial commit

---
+++ src/main/java/smart4viation/task/App.java
@@ -0,0 +1,13 @@
+package smart4viation.task;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
